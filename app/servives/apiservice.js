4c682901379535e9064f53ecbfa3fed69d8cbd19
vijaypanchal05 <vijaypanchal05@gmail.com>
2025-02-17 19:41:09 +0530

    api call  changes.

--- app/servives/apiservice.js
+++ app/servives/apiservice.js
@@ -1,7 +1,10 @@
+// services/apiservie.js
+import userAxiosInstanceWithoutToken from '@/config/userAxiosInstanceWithoutToken'; // Import the axios instance
+
 export const fetchMenuData = async () => {
   try {
-    const response = await fetch('http://localhost:50111/admin/home/dropdown');
-    const data = await response.json();
+    const response = await userAxiosInstanceWithoutToken.get('/admin/home/dropdown'); // Use the Axios instance
+    const data = response.data;
     if (data && data.data) {
       return data.data;
     }
