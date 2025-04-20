389e0fe524b7492da564a7370945fc28b3962738
vijaypanchal05 <vijaypanchal05@gmail.com>
2025-04-20 18:12:06 +0530

    cc

--- components/step-components/step-navigate.tsx
+++ components/step-components/step-navigate.tsx
@@ -81,6 +81,7 @@ export default function StepNavigate({
             </div>
           </div>
         )}
+
         {searchParams.size !== 0 && (
           <div
             className="navigate-item"
