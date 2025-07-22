4d2107a301b17eb72c9078064ae5e2c9fccc06c9
kanha91 <kanhasingh91@gmail.com>
2025-07-23 00:17:13 +0530

    wored on cms pages

--- app/(pages)/(layout)/our-story/page.tsx
+++ app/(pages)/(layout)/our-story/page.tsx
@@ -1,22 +1,41 @@
-import { OurStoryList } from '@/components/our-story';
-// import { URL_SLUG } from '@/constants';
-// import { getBannerBySlug, getStoryPageList } from '@/utils/server-api.utils';
+// import { OurStoryList } from '@/components/our-story';
+// // import { URL_SLUG } from '@/constants';
+// // import { getBannerBySlug, getStoryPageList } from '@/utils/server-api.utils';
 
-export default async function OurStoryPage({
-  // searchParams,
-}: {
-  searchParams: Promise<{ [key: string]: string }>;
-}) {
-  // const resolvedSearchParams = await searchParams;
-  // const page = resolvedSearchParams?.[URL_SLUG.PAGINATION.PAGE] || '1';
+// export default async function OurStoryPage({
+//   // searchParams,
+// }: {
+//   searchParams: Promise<{ [key: string]: string }>;
+// }) {
+//   // const resolvedSearchParams = await searchParams;
+//   // const page = resolvedSearchParams?.[URL_SLUG.PAGINATION.PAGE] || '1';
 
-  // const dropdownRequests = [
-  //   getStoryPageList(+page),
-  //   getBannerBySlug('our-story')
-  // ];
+//   // const dropdownRequests = [
+//   //   getStoryPageList(+page),
+//   //   getBannerBySlug('our-story')
+//   // ];
 
 
-  // Extract results safely
+//   // Extract results safely
 
-  return <OurStoryList />;
+//   return <OurStoryList />;
+// }
+
+
+
+import Head from 'next/head';
+import OurStoryContent from '../../../../components/our-story/OurStoryContent';
+
+export default function OurStoryPage() {
+  return (
+    <>
+      <Head>
+        <title>Our Story | Bespoke Cashmere</title>
+        <meta name="description" content="Discover the story behind our brand" />
+      </Head>
+      <main className="container">
+        <OurStoryContent />
+      </main>
+    </>
+  );
 }
