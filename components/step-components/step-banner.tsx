b532dc4c90baf35569d4027220f3306e9b77c232
unknown <macgw86@gmail.com>
2025-10-20 18:24:30 +0530

    add new page

--- components/step-components/step-banner.tsx
+++ components/step-components/step-banner.tsx
@@ -1,7 +1,7 @@
 import { IBannerData } from '@/types';
-import { getAWSImageUrl } from '@/utils/common.utils';
+// import { getAWSImageUrl } from '@/utils/common.utils';
 import { getBannerBySlug } from '@/utils/server-api.utils';
-import Image from 'next/image';
+// import Image from 'next/image';
 
 export default async function StepBanner({ step }: { stepData?: any; step: string }) {
   const sweaterBannerData = (await getBannerBySlug(`sweater-${step}`)) as IBannerData;
